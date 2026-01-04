a9aada7f51fddc2593d041a952dc14db5ad5480e
Jeisson-Acosta <juanacostamillos69@gmail.com>
2026-01-03 19:41:31 -0500

    feat: I added a new thing how a small description in JSON format

--- src/pages/HomeApp.jsx
+++ src/pages/HomeApp.jsx
@@ -1,6 +1,6 @@
-import { Zap, FileUser  } from 'lucide-react'
+import { Zap, FileUser, Code  } from 'lucide-react'
 import '../styles/pages/HomeApp.css'
-import { OBJECT_DESCRIPTION } from '../constants.js'
+import { OBJECT_DESCRIPTION, JSON_DESCRIPTION } from '../constants.js'
 
 export function HomeApp() {
     return (
@@ -12,7 +12,7 @@ export function HomeApp() {
                     </div>
                     <div style={{paddingLeft: '8px', margin: '3rem', gap: '1.5rem', display: 'flex', flexDirection: 'column'}}>
                         <h2 className='developer-name'>
-                            Hola, soy
+                            Hi, I'm
                             <br />
                             <span className='my-name-gradient'>
                                 Jeisson
@@ -43,6 +43,41 @@ export function HomeApp() {
                             <FileUser style={{width: '20px', height: '20px'}}/>
                             Descargar CV
                         </button>
+                    </div>
+                </div>
+                <div className="info-json">
+                    <div className="container-info-json">
+                        <Code className='code-icon' />
+                        <div style={{margin: '1rem'}}>
+                            <div style={{color: '#22d3ee'}}>{'{'}</div>
+                            {Object.keys(JSON_DESCRIPTION).map((key, index) => {
+                                return (
+                                    <div key={index} style={{paddingLeft: '24px', lineHeight: '2.2rem'}}>
+                                        <span style={{color: "#facc15"}}>{`"${key}"`}</span>
+                                        <span style={{color: '#fff'}}>
+                                            {': ' + (key === 'skills' ? '[' : '')}
+                                            {key === 'available' && <span style={{color: '#60a5fa'}}>{'true'}</span>}
+                                        </span>
+                                        <span style={{color: '#4ade80', lineHeight: '1.4rem'}}>
+                                            {
+                                                key === 'skills' 
+                                                    ? JSON_DESCRIPTION[key].map((skill, index) => {
+                                                        return (
+                                                            <div key={skill} style={{paddingLeft: '24px', marginTop: '4px'}}>
+                                                                <span>
+                                                                    {`"${skill}"` + (index === JSON_DESCRIPTION[key].length - 1 ? '' : ' ,')}
+                                                                </span>
+                                                            </div>
+                                                        )
+                                                }) : JSON_DESCRIPTION[key]
+                                            }
+                                        </span>
+                                        {key === 'skills' && <span style={{color: '#fff'}}>{'],'}</span>}
+                                    </div>
+                                )
+                            })}
+                            <div style={{color: '#22d3ee'}}>{'}'}</div>
+                        </div>
                     </div>
                 </div>
             </section>
